b150a1bf6f12b11df6da0a8a5006e7310cfd1995
emcallaway <callaway@grinnell.edu>
2014-02-12 21:49:28 -0600

    Calculator Test finished

--- src/edu/grinnell/csc207/bresette/fractions/CalculatorTest.java
+++ src/edu/grinnell/csc207/bresette/fractions/CalculatorTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 public class CalculatorTest
 {
-
   @Test
   public void testEvaluate()
     throws Exception
@@ -49,14 +48,20 @@ public class CalculatorTest
     results[2] = "2";
     results[3] = "7/2";
     results[4] = "1/3";
-    
-    assertArrayEquals("Testing expression with results", results, Calculator.evaluate(expression));
-    
-    System.out.println(Calculator.evaluate("r0 = 1 + 2/3"));
-    System.out.println(Calculator.evaluate("r2 = 4 + 3"));
-    System.out.println(Calculator.evaluate("r3 = r2 - 1"));
-    System.out.println(Calculator.evaluate("r3 / 1/2"));
-    
-  }
 
-}
+    assertArrayEquals("Testing expression with results", results,
+                      Calculator.evaluate(expression));
+
+    assertEquals("Testing r0 = 1 + 2/3", "5/3",
+                 Calculator.evaluate("r0 = 1 + 2/3"));
+    assertEquals("Testing r2 = 4 + 3", "7", Calculator.evaluate("r2 = 4 + 3"));
+    assertEquals("Testing r3 = r2 - 1", "6", Calculator.evaluate("r3 = r2 - 1"));
+    assertEquals("Testing r2 = 4 + 3", "12/1", Calculator.evaluate("r3 / 1/2"));
+    assertEquals("Testing r2 = 4 + 3", "7", Calculator.evaluate("r2 = 4 + 3"));
+    assertEquals("Testing r7 = 3/4 * 5", "15/4",
+                 Calculator.evaluate("r7 = 3/4 * 5"));
+    assertEquals("Testing r2 = 45/2 / 4/7", "315/8",
+                 Calculator.evaluate("r4 = 45/2 / 4/7"));
+    assertEquals("Testing r4 * 4/7", "45/2", Calculator.evaluate("r4 * 4/7"));
+  } // testEvaluate()
+} // class CalculatorTest
